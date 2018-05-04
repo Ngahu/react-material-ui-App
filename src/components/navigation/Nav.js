3a7602dfa798166051d8d362d39140cce3140f87
Ngahu <jamaalaraheem@gmail.com>
2018-05-04 21:22:19 +0300

    creating of the navigation bar

---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+   
+ <nav className="navbar navbar-expand-lg navbar-light fixed-top  bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Explore</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About us</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact us</a>
+      </li>
+
+    </ul>  
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+      <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Register</button>
+   
+  </div>
+</nav> 
+
+
+    </div>
+  )
+}
